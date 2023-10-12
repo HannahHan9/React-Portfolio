ea2faafcdf44b852c465c7d2e8ccd655f3b09ca3
HannahHan9 <h.han9204@gmail.com>
2023-10-12 19:19:49 +0100

    update Header and NavBar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,15 @@ import React from "react";
 
 const Header = () => {
     return (
-    <header>
-        <div className="container header__container">
-            <h1>Hi, I'm Hannah</h1>
-            <h3 className="text-light">Junior Software Developer</h3>
-
-        </div>
-    </header>);
+        <header>
+            <div className="text-white flex justify-centre">
+                <h1 className="text-2xl font-workSans font-extrabold">
+                    Hi, I'm Hannah
+                </h1>
+                <h3 className="text-light">Junior Software Developer</h3>
+            </div>
+        </header>
+    );
 };
 
 export default Header;
