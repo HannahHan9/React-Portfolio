95ef2c917b2c7e89f5ebbe45f95ab85833413bed
HannahHan9 <h.han9204@gmail.com>
2023-10-13 09:16:49 +0100

    add Skills component

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,8 +4,8 @@ const Nav = () => {
     return (
         <div className="text-white flex flex-row justify-center font-workSans font-medium mt-10">
             <h1 className="mr-2 md:mr-20">About</h1>
-            <h1 className="mr-2 md:mr-20">Experience</h1>
             <h1 className="mr-2 md:mr-20">Projects</h1>
+            <h1 className="mr-2 md:mr-20">Skills</h1>
             <h1 className="">Contact</h1>
         </div>
     );
---
+++ src/components/Skills.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Skills = () => {
+    return (
+        <div className="pl-3 pr-3 pt-10 bg-[#55E6A5]">
+            <h1>My experience and skills</h1>
+            <p className="p-5"></p>
+        </div>
+    );
+};
+
+export default Skills;
